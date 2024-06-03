0c16a9bc6b5825cbed4ad6c8ea0167c349f3c816
JuhyunSim <simzoo93@naver.com>
2024-06-03 14:29:28 +0900

    fix : [004Partner - Add&GetStore] : storeRepository 메서드 변경

--- partner/src/main/java/com/zerobase/partner/domain/repository/StoreRepository.java
+++ partner/src/main/java/com/zerobase/partner/domain/repository/StoreRepository.java
@@ -10,5 +10,6 @@ import java.util.List;
 public interface StoreRepository extends JpaRepository<StoreEntity, Long> {
     List<StoreEntity> findAllByPartnerIdOrderByNameAsc(Long partnerId);
 
-    boolean findAllByPartnerIdAndName(Long partnerId, String name);
+     boolean existsByPartnerIdAndName(Long partnerId, String name);
+
 }
